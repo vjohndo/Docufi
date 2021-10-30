a40de9294a2b0eeb004d70a44b1b3a7cc52f2ee2
SDiamond <shaunn.diamond@outlook.com>
2021-10-30 15:50:13 +1000

    db updates - use env vars

--- database/db.js
+++ database/db.js
@@ -1,12 +1,15 @@
 const pg = require("pg");
-
+const dotenv = require("dotenv");
+dotenv.config();
 // initialise the variable db:
 let db;
 
 
 if (process.env.NODE_ENV === 'production') {
   db = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     ssl: {
       rejectUnauthorized: false
     }
