cb4ddb76bc911aa13ce2e06739e487937c1929ad
vjohndo <v.john.do@outlook.com>
2022-03-01 00:14:26 +1100

    deploying on heroku

--- database/db.js
+++ database/db.js
@@ -4,14 +4,24 @@ dotenv.config();
 // initialise the variable db:
 let db;
 
-db = new pg.Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  },
-})
+// user: process.env.DB_USER,
+// password: process.env.DB_PASSWORD,
+// host: process.env.DB_HOST,
+//   ssl: {
+//   rejectUnauthorized: false
+// },
+
+if (process.env.NODE_ENV === 'production') {
+  db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+  db = new pg.Pool({
+    database: 'docufi',
+  })
+}
 
 module.exports = db;
