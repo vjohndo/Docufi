6f6c8ef1d25057cfcbb2dba3381d5445f34b0c18
vjohndo <v.john.do@outlook.com>
2022-03-05 01:17:37 +1100

    updated with more instructions, user prompts

--- client/js/initialize.js
+++ client/js/initialize.js
@@ -1,8 +1,8 @@
 window.SOCKET = {};
 SOCKET.socketId = "";
 // local development
-const socket = io("http://localhost:3000");
-// const socket = io.connect("https://docufi-app.herokuapp.com/");
+// const socket = io("http://localhost:3000");
+const socket = io.connect("https://docufi-app.herokuapp.com/");
 
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE, PATCH, OPTIONS';
