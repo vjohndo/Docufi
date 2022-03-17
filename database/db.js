f88e09affa91854e8934794be2e02cb31b809ff3
vjohndo <v.john.do@outlook.com>
2022-03-17 23:21:51 +1100

    reset live site

--- database/db.js
+++ database/db.js
@@ -4,13 +4,6 @@ dotenv.config();
 // initialise the variable db:
 let db;
 
-// user: process.env.DB_USER,
-// password: process.env.DB_PASSWORD,
-// host: process.env.DB_HOST,
-//   ssl: {
-//   rejectUnauthorized: false
-// },
-
 if (process.env.NODE_ENV === 'production') {
   db = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
